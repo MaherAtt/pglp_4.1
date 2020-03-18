40249c38a9433b6c69485992992ed23d7121aaa9
Maher Attouche <maherattouche7@gmail.com>
2020-03-18 13:05:45 +0100

    Classe personnels

---
+++ src/main/java/uvsq/pglp_4_1_/Personnels.java
@@ -0,0 +1,56 @@
+package uvsq.pglp_4_1_;
+
+import java.util.ArrayList;
+
+public final class Personnels implements GroupPersonnels {
+	
+	private  String Nom;
+	private  String Prenom;
+	private  String fonction;
+	private  java.time.LocalDate dateNaissance;
+	private  ArrayList<Integer> numerosDeTel;
+	
+	Personnels(Builder bu)
+	{
+		Nom=bu.Nom;
+		Prenom=bu.Prenom;
+		fonction=bu.fonction;
+		dateNaissance=bu.dateNaissance;
+		numerosDeTel=bu.numerosDeTel;
+	}
+	public static class Builder {
+		
+		private final String Nom;
+		private final String Prenom;
+		private final java.time.LocalDate dateNaissance;
+		
+		private  String fonction="Non Connue";		
+		private  ArrayList<Integer> numerosDeTel=null;
+		
+		
+		public Builder(String Name,String Surname,java.time.LocalDate Birth)
+		{
+			this.dateNaissance=Birth;
+			this.Nom=Name;
+			this.Prenom=Surname;
+		}
+		
+		public Builder AddTel(int numero)
+		{
+			this.numerosDeTel.add(numero);
+			return this;
+		}
+		
+		public Personnels build()
+		{
+			return new Personnels(this);
+		}
+	}
+	public void print() {
+		// TODO Auto-generated method stub
+		System.out.println("Nom:" + Nom );
+		
+	}
+	
+
+}
