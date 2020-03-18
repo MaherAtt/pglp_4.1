a8b641b0d0885b6f54e6703349c904645d042266
Maher Attouche <maherattouche7@gmail.com>
2020-03-18 13:12:39 +0100

    classe Composite personnels

---
+++ src/main/java/uvsq/pglp_4_1_/CompositePersonnels.java
@@ -0,0 +1,29 @@
+package uvsq.pglp_4_1_;
+import java.util.ArrayList;
+public class CompositePersonnels implements GroupPersonnels {
+
+	
+	int id;
+	ArrayList<GroupPersonnels> personnes= new ArrayList<GroupPersonnels>();
+	public CompositePersonnels(int id)
+	{
+		this.id=id;
+	}
+	public void add(GroupPersonnels grp)
+	{
+		personnes.add(grp);
+	}
+	
+	public void remove(GroupPersonnels grp)
+	{
+		personnes.remove(grp);
+	}
+	
+	public void print() {
+		System.out.println(id);
+		for(int k=0;k<personnes.size();k++)
+		{
+			personnes.get(k).print();
+		}
+	}
+}
