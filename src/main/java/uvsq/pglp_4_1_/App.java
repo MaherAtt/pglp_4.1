5e5f54a713901bbdaab70caec01f80fffe6cfae7
Maher Attouche <maherattouche7@gmail.com>
2020-03-18 13:15:12 +0100

    Traitement main avec builder et composite et iterateur

--- src/main/java/uvsq/pglp_4_1_/App.java
+++ src/main/java/uvsq/pglp_4_1_/App.java
@@ -1,5 +1,10 @@
 package uvsq.pglp_4_1_;
 
+import java.time.LocalDate;
+import java.util.Iterator;
+
+import uvsq.pglp_4_1_.Personnels.Builder;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,30 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	Personnels p1=new Personnels(new Builder("Maher1","Attouche",LocalDate.now()));
+    	Personnels p4=new Personnels(new Builder("Maher4","Attouche",LocalDate.now()));
+    	Personnels p5=new Personnels(new Builder("Maher5","Attouche",LocalDate.now()));
+
+    	CompositePersonnels cp3=new CompositePersonnels(3);
+    	CompositePersonnels cp2=new CompositePersonnels(2);
+    	
+    	
+    	
+    	cp2.add(p4);
+    	cp2.add(p5);
+    	cp3.add(p1);
+    	cp3.add(cp2);
+    	
+    	//cp3.print();
+    	
+    	AffichageGroup affgrp= new AffichageGroup(cp3);
+    	
+    	for(Iterator<AffichageGroup> i=affgrp.iterator();i.hasNext();)
+    	{
+    	    ((GroupPersonnels) i.next()).print();
+    	}
+
+    	
+    	
     }
 }
